f2af2998140bc1862ca91c3ee549c44e170214a0
daniel155158 <daniel155158@gmail.com>
2022-11-18 22:04:56 +0800

    feat: add mongodb/mongoose connection settings

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const generateShortenURL = require('./generate_shorten_url')
+const url = require('./models/url')
 const port = 3000
+
+//非production環境使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+//MONGOOSE CONNECT
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('MongoDB error')
+})
+db.once('open', () => {
+  console.log('MongoDB connected')
+})
 
 //Setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
---
+++ models/url.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+  url_origin: {
+    type: String,
+    required: true
+  },
+  url_shorten: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Url', urlSchema)
