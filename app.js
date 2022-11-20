318a3494b1db7360445d6b1a0a9df6fa341501ff
daniel155158 <daniel155158@gmail.com>
2022-11-20 09:01:28 +0800

    refactor: add mongoose config

--- app.js
+++ app.js
@@ -2,24 +2,10 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+require('./config/mongoose')
 const generateShortenURL = require('./generate_shorten_url')
 const Url = require('./models/url')
 const port = 3000
-
-//非production環境使用dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-//MONGOOSE CONNECT
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('MongoDB error')
-})
-db.once('open', () => {
-  console.log('MongoDB connected')
-})
 
 //Setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
